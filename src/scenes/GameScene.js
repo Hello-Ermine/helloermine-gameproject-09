46639ba90c4796dfa94c4e25657edb301f494048
panithee <panithee.pnt@mail.kmutt.ac.th>
2021-11-15 21:18:41 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,8 +3,11 @@ import Phaser from "phaser";
 let bg;
 let keyA;
 let keyD;
-let object1;    
+let keySPACE;
+let object1;
 let player;
+let playerJump;
+
 
 class GameScene extends Phaser.Scene {
     constructor(test) {
@@ -15,31 +18,38 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('bg', 'src/image/scene1.jpg');
+
         this.load.spritesheet('player', 'src/image/walk.png',
-                {frameWidth:1024, frameHeight:1714});
+            { frameWidth: 1024, frameHeight: 1714 });
+        this.load.spritesheet('playerJump', 'src/image/jumpnew.png',
+            { frameWidth: 1024, frameHeight: 1049 });
+
+
 
         this.load.image('object1', 'src/image/object/1.png');
     }
 
     create() {
-        
+
         //background
-        bg = this.add.tileSprite(0,0,700,950,'bg').setOrigin(0,0);
-        
+        bg = this.add.tileSprite(0, 0, 700, 950, 'bg').setOrigin(0, 0);
+
         //object
-        object1 = this.physics.add.image(0,932, 'object1')
-        .setOrigin(0,0)
-        .setImmovable()
-        .setSize(1500,0)
-        .setOffset(0,14);
-        this.physics.add.collider(object1, player);
+        object1 = this.physics.add.image(0, 932, 'object1')
+            .setOrigin(0, 0)
+            .setImmovable()
+            .setSize(1500, 0)
+            .setOffset(0, 14);
+
 
         //player
-        player = this.physics.add.sprite(225, 700, 'player');
-        player.setGravityY(650);
-        player.setScale(0.07);
-        player.setSize(1000,990);
-        player.setOffset(0,400);
+        this.player = this.physics.add.sprite(225, 700, 'player');
+        this.player.setGravityY(650);
+        this.player.setScale(0.07);
+        this.player.setSize(1000, 990);
+        this.player.setOffset(0, 400);
+
+
 
         //animation player
         this.anims.create({
@@ -48,7 +58,7 @@ class GameScene extends Phaser.Scene {
                 start: 0,
                 end: 3
             }),
-            duration: 500,    
+            duration: 500,
             repeat: -1
         })
 
@@ -58,28 +68,48 @@ class GameScene extends Phaser.Scene {
                 start: 0,
                 end: 0
             }),
-            duration: 500,    
+            duration: 500,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'playerChargeJumpAni',
+            frames: this.anims.generateFrameNumbers('playerJump', {
+                start: 0,
+                end: 0
+            }),
+            duration: 500,
+            repeat: -1
+        })
+
+        this.anims.create({
+            key: 'playerJumpAni',
+            frames: this.anims.generateFrameNumbers('playerJump', {
+                start: 1,
+                end: 2
+            }),
+            duration: 500,
             repeat: -1
         })
 
 
 
+        this.player.setCollideWorldBounds(true)
 
-        player.setCollideWorldBounds(true)
-
-        this.input.on('pointerdown',this.startJump, player);
-        this.input.on('pointerup',this.endJump, player);
+        keySPACE = this.input.keyboard.on('keydown_SPACE',this.startJump, this);
+        keySPACE = this.input.keyboard.on('keyup_SPACE',this.endJump, this);
 
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-
+        this.physics.add.collider(object1, this.player);
 
         this.power = 0;
     }
     endJump() {
         this.timer.remove();
-        player.setVelocityY(-this.power * 130);
+        this.player.setVelocityY(-this.power * 130);
         this.power = 0;
     }
 
@@ -87,8 +117,8 @@ class GameScene extends Phaser.Scene {
         this.timer = this.time.addEvent({ delay: 100, callback: this.charge, callbackScope: this, loop: true });
 
     }
-    charge(){
-        if(this.power <= 2.5){
+    charge() {
+        if (this.power <= 3.5) {
             this.power += .3;
             console.log(this.power);
         }
@@ -97,23 +127,30 @@ class GameScene extends Phaser.Scene {
 
     update(delta, time) {
 
+
         //playerwalk + velocity + anims
-       if(keyA.isDown){
-           player.setVelocityX(-300);
-           player.anims.play('playerWalkAni', true);
-           player.flipX=true;
-           
-       }else if(keyD.isDown){
-           player.setVelocityX(300);
-           player.anims.play('playerWalkAni', true);
-           player.flipX=false;
-       }else{
-           player.setVelocityX(0);
-           player.anims.play('playerIdleAni', true);
-       }
+        if (keyA.isDown) {
+            this.player.setVelocityX(-300);
+            this.player.anims.play('playerWalkAni', true);
+            this.player.flipX = true;
+
+        } else if (keyD.isDown) {   
+            this.player.setVelocityX(300);
+            this.player.anims.play('playerWalkAni', true);
+            this.player.flipX = false;
+        } else if (keySPACE.isDown) {
+            this.player.anims.play('playerChargeJumpAni', true);
+            this.player.setVelocityX(0);
+        } else if (Phaser.Input.Keyboard.JustUp(keySPACE)) {
+            this.player.anims.play('playerJumpAni', true);
+            
+        } else{
+            this.player.setVelocityX(0);
+            this.player.anims.play('playerIdleAni', true);
+        }
+
     }
 
-       
 
 }
 export default GameScene;
