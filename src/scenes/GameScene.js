972c5ebe7ce400d791bff79dfd4b8ffbeeee7db0
panithee <panithee.pnt@mail.kmutt.ac.th>
2021-11-15 19:12:40 +0700

    walkAnima

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -3,6 +3,7 @@ import Phaser from "phaser";
 let bg;
 let keyA;
 let keyD;
+let player;
 
 
 class GameScene extends Phaser.Scene {
@@ -15,7 +16,7 @@ class GameScene extends Phaser.Scene {
     preload() {
         this.load.image('bg', 'src/image/bgmain.png');
         this.load.spritesheet('player', 'src/image/walk.png',
-                {frameWidth:1024, frameHeight:1714});
+                {frameWidth:368, frameHeight:368});
 
     }
 
@@ -24,9 +25,9 @@ class GameScene extends Phaser.Scene {
         //background
         bg = this.add.tileSprite(0,-4150,700,4900,'bg').setOrigin(0,0);
         
-        this.player = this.physics.add.image(225, 700, 'player');
+        this.player = this.physics.add.sprite(225, 700, 'player');
         this.player.setGravityY(400);
-        this.player.setScale(0.175);
+        this.player.setScale(0.4);
 
 
         //animation player
@@ -47,13 +48,11 @@ class GameScene extends Phaser.Scene {
 
         this.input.on('pointerdown', this.startJump, this);
         this.input.on('pointerup', this.endJump, this);
+
         keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 
         // this.input.keyboard.on('keyup_RIGHT', this.rightjump, this);
-
-
-
 
 
         this.power = 0;
@@ -69,19 +68,23 @@ class GameScene extends Phaser.Scene {
 
     }
 
-    create() {
-    bg = this.add.tileSprite(0,-4150,700,4900, 'bg').setOrigin(0,0);
-    }
+    // create() {
+    // bg = this.add.tileSprite(0,-4150,700,4900, 'bg').setOrigin(0,0);
+    // }
 
     update(delta, time) {
-        if (keyA.isDown) {
-            this.player.setVelocityX(-300);
-            this.player.anims.play('playerWalkAni', true);
-        } else if (keyD.isDown) {
-            this.player.setVelocityX(300);
-        } else {
-            this.player.setVelocityX(0);
-        }
+
+       if(keyA.isDown){
+           this.player.setVelocityX(-300);
+           this.player.anims.play('playerWalkAni', true);
+           
+       }else if(keyD.isDown){
+           this.player.setVelocityX(300);
+           this.player.anims.play('playerWalkAni', true);
+       }else{
+           this.player.setVelocityX(0);
+       }
     }
+
 }
 export default GameScene;
